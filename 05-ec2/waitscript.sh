80b55091c929a4110c2342773ec23e6ea7fa542e
Greyson Gundrum <greyson.gundrum@stelligent.com>
2022-10-08 20:46:24 -0700

    I FINALLY FINSHED MODULE 5

--- 05-ec2/waitscript.sh
+++ 05-ec2/waitscript.sh
@@ -5,7 +5,7 @@ echo "Delete command Issued"
 aws cloudformation wait stack-delete-complete --stack-name greysongundrum$CURRENT_LAB 
 echo "Deletion Complete"
 echo "Issuing Creation Command"
-aws cloudformation create-stack --stack-name greysongundrum$CURRENT_LAB --template-body "file://$PWD/$CURRENT_LAB.yaml"
+aws cloudformation create-stack --stack-name greysongundrum$CURRENT_LAB --template-body "file://$PWD/$CURRENT_LAB.yaml" --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
 echo  "Create Command Sent"
 echo "Waiting for Stack Creation to Complete"
 aws cloudformation wait stack-create-complete --stack-name greysongundrum$CURRENT_LAB
